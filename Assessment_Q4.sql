3d17ea98c077b9a921b510a6472a09ed641c21c6
Bukola <159951223+BukolaHaruna@users.noreply.github.com>
2025-05-18 21:52:27 +0100

    Update Assessment_Q4.sql

--- Assessment_Q4.sql
+++ Assessment_Q4.sql
@@ -1,5 +1,4 @@
 # Assessment_Q4.sql
-# Where needed, 'u', 's' and 'p' were used as aliases for users_customuser, savings_savingsaccount and plans_plan tables respectively for easy querying.
 
 SELECT
     u.id AS customer_id,
@@ -7,12 +6,11 @@ SELECT
     CONCAT(u.first_name, ' ', u.last_name) AS name,
     TIMESTAMPDIFF(MONTH, u.date_joined, CURDATE()) AS tenure_months,
     COUNT(s.id) AS total_transactions,
-# Round the estimated CLV to 2 decimal places.
-# Multiply the average confirmed amount by 0.1%.
+# I multiplied the average confirmed amount by 0.1% and rounded the estimated CLV to 2 decimal places.
     ROUND((COUNT(s.id) / TIMESTAMPDIFF(MONTH, u.date_joined, CURDATE())) * 12 * (0.001 * AVG(s.confirmed_amount)), 2) AS estimated_clv
 FROM
     users_customuser AS u
-# Join the savings_savingsaccount table on the owner_ID.
+# Made use of Join to combine the savings_savingsaccount table on the owner_ID.
 JOIN
     savings_savingsaccount s ON u.id = s.owner_id
 GROUP BY
