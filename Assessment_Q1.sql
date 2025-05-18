41c7cccd71afa522488055ba779b62adfc006701
Bukola <159951223+BukolaHaruna@users.noreply.github.com>
2025-05-18 22:07:45 +0100

    Update Assessment_Q1.sql

--- Assessment_Q1.sql
+++ Assessment_Q1.sql
@@ -3,7 +3,7 @@
 SELECT
     u.id AS owner_id,
     
-# The CONCAT function below combines the first_name and last_name columns from the users_customuser table as name.
+# The concat function below combines the first_name and last_name columns from the users_customuser table as name.
     CONCAT(u.first_name, ' ', u.last_name) AS name, 
     COUNT(DISTINCT CASE WHEN p.is_regular_savings = 1 THEN p.id ELSE NULL END) AS savings_count,
     COUNT(DISTINCT CASE WHEN p.is_a_fund = 1 THEN p.id ELSE NULL END) AS investment_count,
@@ -20,7 +20,7 @@ WHERE s.confirmed_amount > 0
 GROUP BY
     u.id, u.name                  
     
-# Using the HAVING clause to filter for users who have at least one of each plan type.
+# I used the having clause to filter for users who have at least one of each plan type.
 HAVING
     COUNT(DISTINCT CASE WHEN p.is_regular_savings = 1 THEN p.id ELSE NULL END) >= 1
     AND COUNT(DISTINCT CASE WHEN p.is_a_fund = 1 THEN p.id ELSE NULL END) >= 1
